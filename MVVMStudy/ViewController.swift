35b6516c85399b350697dc0397649f955e665e50
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-04 23:14:12 +0300

    Initial Commit

---
+++ MVVMStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 4.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
